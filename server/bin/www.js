8e1f801e689a8d7fa19073b35936ed6a5d6ed580
Diana Tinoco <dtinoco152@gmail.com>
2022-05-29 10:10:10 -0500

    Modernizacion del Generador de Express

--- server/bin/www.js
+++ server/bin/www.js
@@ -4,93 +4,89 @@
  * Module dependencies.
  */
 
- // var app = require('../app');
- import app from '../app';
- var debug = require('debug')('projnotes:server');
- var http = require('http');
- const { colors } = require('debug/src/browser');
+ import app from "../app";
+ import Debug from "debug";
+ import http from "http";
+ 
+ // Creando instancia del debugger
+ const debug = Debug("prof:server");
  
  /**
   * Get port from environment and store in Express.
   */
- /**process.env.PORT Forma de acceder a la variable de entorno */
- var port = normalizePort(process.env.PORT || '3000');
- // app es una instancia de ExpressJs[ ] [ NODE ]
- app.set('port', port);
+ 
+ const port = normalizePort(process.env.PORT || "3000");
+ // app es una instnacia de ExpressJs[ ] [ NODE ]
+ app.set("port", port);
  
  /**
   * Create HTTP server.
   */
  
- var server = http.createServer(app); // (req, res, next, err) => {}
+ const server = http.createServer(app); // (req, res, next, err)=> {}
  
  /**
   * Listen on provided port, on all network interfaces.
   */
  
  server.listen(port); // Pone al server a escuchar
- // Se registran eventos 
- server.on('error', onError);
- server.on('listening', onListening);
+ // Se registran eventos
+ server.on("error", onError); // En caso de error
+ server.on("listening", onListening); // Cuando esta escuchando
  
  /**
   * Normalize a port into a number, string, or false.
   */
  
  function normalizePort(val) {
-   var port = parseInt(val, 10);
+   const port = parseInt(val, 10);
  
    if (isNaN(port)) {
      // named pipe
      return val;
    }
- 
+
    if (port >= 0) {
      // port number
      return port;
    }
- 
+
    return false;
  }
- 
+
  /**
   * Event listener for HTTP server "error" event.
   */
  
  function onError(error) {
-   if (error.syscall !== 'listen') {
+   if (error.syscall !== "listen") {
      throw error;
    }
  
-   var bind = typeof port === 'string'
-     ? 'Pipe ' + port
-     : 'Port ' + port;
+   var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
  
    // handle specific listen errors with friendly messages
    switch (error.code) {
-     case 'EACCES':
-       console.error(bind + ' requires elevated privileges');
+     case "EACCES":
+       console.error(`${bind} requires elevated privileges`);
        process.exit(1);
        break;
-     case 'EADDRINUSE':
-       console.error(bind + ' is already in use');
+     case "EADDRINUSE":
+       console.error(`${bind} is already in use`);
        process.exit(1);
        break;
      default:
        throw error;
    }
  }
- 
+
  /**
   * Event listener for HTTP server "listening" event.
   */
  
  function onListening() {
-   var addr = server.address();
-   var bind = typeof addr === 'string'
-     ? 'pipe ' + addr
-     : 'port ' + addr.port;
-   debug('Listening on ' + bind);
-   console.log(`💫 Servidor escuchando... en ${app.get('port')}`)
- 
+   const addr = server.address();
+   const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+   debug(`Listening on ${bind}`);
+   console.log(`Servidor escuchando 💯...en ${app.get("port")}`);
  }
